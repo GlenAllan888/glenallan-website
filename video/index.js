bbc6bc47fcc9168c4af5f9b0a1eacd90f2a295d2
The Chaos System <37121971+GlenAllan888@users.noreply.github.com>
2026-06-24 19:11:46 -0700

    CSS Edits

---
+++ video/index.js
@@ -0,0 +1,10 @@
+
+
+var RWElements={};
+
+RWElements.rw0BE8F1C4_1317_4D4A_AB51_7CB999440F60 = {};
+RWElements.rw0BE8F1C4_1317_4D4A_AB51_7CB999440F60 = (function(componentId) {
+    
+console.log(`Running JS for element with id: `)
+
+return componentId;})(RWElements.rw0BE8F1C4_1317_4D4A_AB51_7CB999440F60);
