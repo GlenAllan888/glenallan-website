1f00aac213537a95fe9375bc97335a620da6d3af
The Chaos System <37121971+GlenAllan888@users.noreply.github.com>
2026-06-25 19:35:00 -0700

    Image css testing

---
+++ design/index.js
@@ -0,0 +1,10 @@
+
+
+var RWElements={};
+
+RWElements.rwF64D92A8_6C77_4151_A35B_ABC9E60368A8 = {};
+RWElements.rwF64D92A8_6C77_4151_A35B_ABC9E60368A8 = (function(componentId) {
+    
+console.log(`Running JS for element with id: `)
+
+return componentId;})(RWElements.rwF64D92A8_6C77_4151_A35B_ABC9E60368A8);
