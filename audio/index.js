a7aa159748af1a2432561e0ac1818567e8120ff1
The Chaos System <37121971+GlenAllan888@users.noreply.github.com>
2026-06-29 07:40:15 -0700

    Big update, added Audio page as well!

---
+++ audio/index.js
@@ -0,0 +1,10 @@
+
+
+var RWElements={};
+
+RWElements.rw8CC77A88_5436_4D93_9FC0_B18BF1BE9042 = {};
+RWElements.rw8CC77A88_5436_4D93_9FC0_B18BF1BE9042 = (function(componentId) {
+    
+console.log(`Running JS for element with id: `)
+
+return componentId;})(RWElements.rw8CC77A88_5436_4D93_9FC0_B18BF1BE9042);
