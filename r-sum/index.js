a9ae9330682249036fa60768d2a48b5115c688be
The Chaos System <37121971+GlenAllan888@users.noreply.github.com>
2026-07-03 22:58:27 -0700

    Full rebuild to consolodate files and clean up the file structure

---
+++ r-sum/index.js
@@ -0,0 +1,10 @@
+
+
+var RWElements={};
+
+RWElements.rw8F13CEF3_7BD8_4F50_A134_304EEDB8BA56 = {};
+RWElements.rw8F13CEF3_7BD8_4F50_A134_304EEDB8BA56 = (function(componentId) {
+    
+console.log(`Running JS for element with id: `)
+
+return componentId;})(RWElements.rw8F13CEF3_7BD8_4F50_A134_304EEDB8BA56);
