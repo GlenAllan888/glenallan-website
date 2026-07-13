0b1c3f582efcf649f02bed3e09954feb2f00ddee
The Chaos System <37121971+GlenAllan888@users.noreply.github.com>
2026-07-12 19:46:01 -0700

    Gallry Pro Lightbox issue page build

---
+++ gallery-pro-issue/index.js
@@ -0,0 +1,10 @@
+
+
+var RWElements={};
+
+RWElements.rw3148E614_2E18_4352_9BDB_3C27E7850F9C = {};
+RWElements.rw3148E614_2E18_4352_9BDB_3C27E7850F9C = (function(componentId) {
+    
+console.log(`Running JS for element with id: `)
+
+return componentId;})(RWElements.rw3148E614_2E18_4352_9BDB_3C27E7850F9C);
