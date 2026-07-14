c6752d561c12fdbbc48eb9f997d8e6343572356d
The Chaos System <37121971+GlenAllan888@users.noreply.github.com>
2026-07-13 20:32:57 -0700

    resume index renaming test

--- video/index.js
+++ video/index.js
@@ -2,9 +2,9 @@
 
 var RWElements={};
 
-RWElements.rw0BE8F1C4_1317_4D4A_AB51_7CB999440F60 = {};
-RWElements.rw0BE8F1C4_1317_4D4A_AB51_7CB999440F60 = (function(componentId) {
+RWElements.rw480BF423_EE9F_4330_B027_2569B431917A = {};
+RWElements.rw480BF423_EE9F_4330_B027_2569B431917A = (function(componentId) {
     
 console.log(`Running JS for element with id: `)
 
-return componentId;})(RWElements.rw0BE8F1C4_1317_4D4A_AB51_7CB999440F60);
+return componentId;})(RWElements.rw480BF423_EE9F_4330_B027_2569B431917A);
