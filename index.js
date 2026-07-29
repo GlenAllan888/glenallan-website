c58faceb83b63b0482c74a00326a78428973f4a8
The Chaos System <37121971+GlenAllan888@users.noreply.github.com>
2026-07-29 08:11:45 -0700

    Home page fade in removed

--- index.js
+++ index.js
@@ -2,48 +2,6 @@
 
 var RWElements={};
 
-RWElements.rw117A718D_7DDE_4F64_A52B_8E6CEA3D9B79 = {};
-RWElements.rw117A718D_7DDE_4F64_A52B_8E6CEA3D9B79 = (function(componentId) {
-    
-// Fade-In Body - Free
-
-const FADE_EFFECT_ENABLED = true;
-const MIN_SCREEN_WIDTH = 100;
-const BODY_DURATION = 1512;
-const BODY_DELAY = 150;
-const BODY_EASING = 'ease';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const body = document.body;
-
-    // fallback safety
-    if (!body) return;
-
-    // no effect
-    if (!FADE_EFFECT_ENABLED || window.innerWidth < MIN_SCREEN_WIDTH) {
-        body.style.opacity = '1';
-        body.style.willChange = 'auto';
-        return;
-    }
-
-    // prepare transition once
-    body.style.transition = `opacity ${BODY_DURATION}ms ${BODY_EASING}`;
-
-    // reveal
-    window.requestAnimationFrame(() => {
-        setTimeout(() => {
-            body.style.opacity = '1';
-
-            // cleanup after animation
-            setTimeout(() => {
-                body.style.willChange = 'auto';
-            }, BODY_DURATION);
-        }, BODY_DELAY);
-    });
-});
-// Copyright ®Multithemes Proprietary code, reuse to create competing products (e.g. Elements App martket) is prohibited.
-
-return componentId;})(RWElements.rw117A718D_7DDE_4F64_A52B_8E6CEA3D9B79);
 RWElements.rw7CA6A693_A0FF_453C_BB92_DF169FB802D0 = {};
 RWElements.rw7CA6A693_A0FF_453C_BB92_DF169FB802D0 = (function(componentId) {
     
