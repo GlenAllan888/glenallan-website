f4c2d5606619aa1debf6f0601ee9e0f03f50c7ff
The Chaos System <37121971+GlenAllan888@users.noreply.github.com>
2026-08-03 22:14:06 -0700

    Added ??? page

---
+++ Info/index.js
@@ -0,0 +1,10 @@
+
+
+var RWElements={};
+
+RWElements.rw4FEF527D_87FE_48CB_978E_C8EDE2C84291 = {};
+RWElements.rw4FEF527D_87FE_48CB_978E_C8EDE2C84291 = (function(componentId) {
+    
+console.log(`Running JS for element with id: `)
+
+return componentId;})(RWElements.rw4FEF527D_87FE_48CB_978E_C8EDE2C84291);
